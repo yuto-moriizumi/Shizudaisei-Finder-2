21f855b70f4d30a67da8304067a86a8318115988
yuto-moriizumi <moriizumi.yuto.18@shizuoka.ac.jp>
2021-04-11 04:41:32 +0900

    ターゲットをES2017に変更

--- front/src/components/UserCard/UserCard.tsx
+++ front/src/components/UserCard/UserCard.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import {
-  Card,
-  Col,
-  Row,
-  Image,
-  Button,
-  Container,
-} from 'react-bootstrap';
+import { Card, Col, Row, Image, Button, Container } from 'react-bootstrap';
 import dayjs from 'dayjs';
 import User from '../../utils/User.d';
 import ButtonState from './ButtonState';
@@ -23,7 +16,7 @@ export default class UserCard extends React.Component<Props, {}> {
   render() {
     const { user } = this.props;
     let { onFollow } = this.props;
-    if(onFollow) onFollow = onFollow.bind(this);
+    if (onFollow) onFollow = onFollow.bind(this);
 
     // フォロー試行状態を判別する
     let buttonState: ButtonState;
@@ -32,17 +25,23 @@ export default class UserCard extends React.Component<Props, {}> {
     else if (user.is_following) buttonState = new FollowedState();
     else buttonState = new BeforeFollowState();
 
-    return (      
+    return (
       <Card key={user.id} className="mb-4">
         <Card.Header className="p-2">
           <Container fluid>
             <Row>
               <Col className="px-0" xs="2">
-                <Image fluid src={user.img_url} style={{ maxHeight: '48px' }} />
+                <Image
+                  fluid
+                  src={user.img_url}
+                  style={{ maxHeight: '48px' }}
+                  width="48px"
+                  height="48px"
+                />
               </Col>
               <Col className="pl-1 pr-0" xs="10">
                 <Card.Link
-                  href={`https://twitter.com/${  user.screen_name}`}
+                  href={`https://twitter.com/${user.screen_name}`}
                   target="_blank"
                 >
                   <h6 className="mb-0">{user.name}</h6>
@@ -64,11 +63,7 @@ export default class UserCard extends React.Component<Props, {}> {
                   size="sm"
                   variant={buttonState.variant}
                   disabled={buttonState.disabled}
-                  onClick={
-                    buttonState.disabled
-                      ? undefined
-                      : onFollow
-                  }
+                  onClick={buttonState.disabled ? undefined : onFollow}
                 >
                   {buttonState.display}
                 </Button>
