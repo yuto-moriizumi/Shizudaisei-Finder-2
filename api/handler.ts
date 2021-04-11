beb26cc2e0a8799582659e7f4472840ee46eb9d7
yuto-moriizumi <moriizumi.yuto.18@shizuoka.ac.jp>
2021-04-11 18:29:49 +0900

    ハンドラexportに注釈を描いた

--- api/handler.ts
+++ api/handler.ts
@@ -1,5 +1,6 @@
 import serverless from 'serverless-http';
 import app from './app';
 
-const myhandler = serverless(app);
-export default myhandler;
+// デフォルトexportだと、handlerを見つけられなくなる
+// eslint-disable-next-line import/prefer-default-export
+export const myhandler = serverless(app);
